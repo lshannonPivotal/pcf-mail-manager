f76a75612a2a1d9e3c37528366f59be07f19c03c
lshannon <luke.shannon@gmail.com>
2015-07-23 19:52:15 -0400

    Check this in accidentially, deleted it

--- mail-ui/src/main/java/io/pivotal/fe/pcf/mailmanager/ui/MailUiApplication.java
+++ mail-ui/src/main/java/io/pivotal/fe/pcf/mailmanager/ui/MailUiApplication.java
@@ -1,12 +1,12 @@
-package demo;
+package io.pivotal.fe.pcf.mailmanager.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MailUiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MailUiApplication.class, args);
     }
 }
--- mail-ui/src/test/java/io/pivotal/fe/pcf/mailmanager/ui/MailUiApplicationTests.java
+++ mail-ui/src/test/java/io/pivotal/fe/pcf/mailmanager/ui/MailUiApplicationTests.java
@@ -1,13 +1,15 @@
-package demo;
+package io.pivotal.fe.pcf.mailmanager.ui;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
-public class DemoApplicationTests {
+@SpringApplicationConfiguration(classes = MailUiApplication.class)
+@WebAppConfiguration
+public class MailUiApplicationTests {
 
 	@Test
 	public void contextLoads() {
